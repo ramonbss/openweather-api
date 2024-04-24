55442f86eb1ca9cac7404c4ea0f24f936ac9eca2
Ramon Brito <ramon.mecatec@gmail.com>
2024-04-24 17:33:33 -0300

    Implementar previsao de tempo; Corrigir fixtures cidade e api_key

--- openweather.py
+++ openweather.py
@@ -3,6 +3,7 @@ import requests
 
 class OpenWeather:
     ENDPOINT_GEOLOCALIZACAO = "http://api.openweathermap.org/geo/1.0/direct?"
+    ENDPOINT_PREVISAO_DE_TEMPO = "https://api.openweathermap.org/data/3.0/onecall?"
 
     def __init__(self, api_key: str) -> None:
         self._api_key = api_key
@@ -19,8 +20,30 @@ class OpenWeather:
             )
             return None, None
 
+    def obter_previsao_do_tempo_8_dias(self, cidade: str):
+        """
+        Obtem a previsão de tempo do dia atual e dos próximos 8 dias
+         para a cidade escolhida
+        """
+        latitude, longitude = self.obter_latitude_longitude(cidade)
+        if latitude is None or longitude is None:
+            return None
+
+        url_api_previsao_do_tempo = self.construir_url_api_previsao_tempo(
+            latitude, longitude
+        )
+        resposta = requests.get(url_api_previsao_do_tempo)
+        if resposta.status_code == 200:
+            return resposta.json()
+        else:
+            print(f"Error fetching weather data: {resposta.status_code}")
+            return None
+
     def construir_url_api_geolocalizacao(self, cidade):
         return f"{self.ENDPOINT_GEOLOCALIZACAO}q={cidade}&limit=1&appid={self._api_key}"
+
+    def construir_url_api_previsao_tempo(self, latitude: int, longitude: int):
+        return f"{self.ENDPOINT_PREVISAO_DE_TEMPO}lat={latitude}&lon={longitude}&lang=pt_br&exclude=minutely,hourly,alerts&appid={self._api_key}&units=metric"
 
 
 if __name__ == "__main__":
@@ -30,4 +53,4 @@ if __name__ == "__main__":
     load_dotenv()
     api_key = os.getenv("API_KEY")
     openweathter = OpenWeather(api_key)
-    openweathter.obter_latitude_longitude("Fortaleza")
+    openweathter.obter_previsao_do_tempo_8_dias("Salvador")
--- test_openweather_sdk.py
+++ test_openweather_sdk.py
@@ -11,7 +11,12 @@ load_dotenv()
 
 @pytest.fixture
 def api_key():
-    return os.getenv("API_KEY")
+    return "api_falsa"
+
+
+@pytest.fixture
+def cidade():
+    return "Salvador"
 
 
 @pytest.fixture
@@ -20,11 +25,10 @@ def openweather(api_key):
 
 
 @pytest.fixture
-def resposta_api_geolocalizacao():
-    """Sample JSON response that mimics the OpenWeatherMap Geocoding API response for testing."""
+def resposta_api_geolocalizacao(cidade):
     return [
         {
-            "name": "Salvador",
+            "name": cidade,
             "lat": -12.9822499,
             "lon": -38.4812772,
             "country": "BR",
@@ -33,8 +37,23 @@ def resposta_api_geolocalizacao():
     ]
 
 
-def test_api_key(api_key):
-    assert api_key is not None
+@pytest.fixture
+def resposta_api_previsao_tempo():
+    return {
+        "current": {"temp": 28, "weather": [{"description": "nuvens dispersas"}]},
+        "daily": [
+            {"dt": 1713987437, "temp": {"day": 28}},
+            {"dt": 1714073837, "temp": {"day": 29}},
+            {"dt": 1714160237, "temp": {"day": 24}},
+            {"dt": 1714246637, "temp": {"day": 26}},
+            {"dt": 1714333037, "temp": {"day": 25}},
+            {"dt": 1714419437, "temp": {"day": 25}},
+        ],
+    }
+
+
+def test_leitura_api_key():
+    assert os.getenv("API_KEY") is not None
 
 
 def test_endpoint_geolocalizacao_preenchido(openweather):
@@ -42,11 +61,32 @@ def test_endpoint_geolocalizacao_preenchido(openweather):
     assert len(openweather.ENDPOINT_GEOLOCALIZACAO) > 0
 
 
-def test_obter_latitude_longitude(openweather, resposta_api_geolocalizacao):
+def test_endpoint_previsao_temperatura_preenchido(openweather):
+    assert openweather.ENDPOINT_PREVISAO_DE_TEMPO is not None
+    assert len(openweather.ENDPOINT_PREVISAO_DE_TEMPO) > 0
+
+
+def test_obter_latitude_longitude(openweather, resposta_api_geolocalizacao, cidade):
     with requests_mock.Mocker() as mocker:
-        cidade = "Salvador"
         geocode_url = openweather.construir_url_api_geolocalizacao(cidade)
         mocker.get(geocode_url, json=resposta_api_geolocalizacao)
         lat, lon = openweather.obter_latitude_longitude(cidade)
         assert lat == -12.9822499
         assert lon == -38.4812772
+
+
+def test_obter_previsao_tempo(
+    openweather, cidade, resposta_api_geolocalizacao, resposta_api_previsao_tempo
+):
+    with requests_mock.Mocker() as mocker:
+        url_geolocalizacao = openweather.construir_url_api_geolocalizacao(cidade)
+        latitude = resposta_api_geolocalizacao[0]["lat"]
+        longitude = resposta_api_geolocalizacao[0]["lon"]
+        url_previsao_de_tempo = openweather.construir_url_api_previsao_tempo(
+            latitude, longitude
+        )
+        mocker.get(url_geolocalizacao, json=resposta_api_geolocalizacao)
+        mocker.get(url_previsao_de_tempo, json=resposta_api_previsao_tempo)
+        dados_previsoes = openweather.obter_previsao_do_tempo_8_dias(cidade)
+        assert dados_previsoes is not None
+        assert dados_previsoes["current"]["temp"] == 28
