4d9a7842d224cab29e5abfb17813467ffaf44b35
Ramon Brito <ramon.mecatec@gmail.com>
2024-04-24 16:04:14 -0300

    Implementar geolocalizacao

--- openweather.py
+++ openweather.py
@@ -1,1 +1,33 @@
-class OpenWeather: ...
+import requests
+
+
+class OpenWeather:
+    ENDPOINT_GEOLOCALIZACAO = "http://api.openweathermap.org/geo/1.0/direct?"
+
+    def __init__(self, api_key: str) -> None:
+        self._api_key = api_key
+
+    def obter_latitude_longitude(self, cidade: str):
+        url_api_geolocalizacao = self.construir_url_api_geolocalizacao(cidade)
+        resposta = requests.get(url_api_geolocalizacao)
+        if resposta.status_code == 200 and resposta.json():
+            data = resposta.json()[0]
+            return data["lat"], data["lon"]
+        else:
+            print(
+                f"Erro '{resposta.status_code}' ao obter localização da cidade '{cidade}'"
+            )
+            return None, None
+
+    def construir_url_api_geolocalizacao(self, cidade):
+        return f"{self.ENDPOINT_GEOLOCALIZACAO}q={cidade}&limit=1&appid={self._api_key}"
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    import os
+
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+    openweathter = OpenWeather(api_key)
+    openweathter.obter_latitude_longitude("Fortaleza")
--- test_openweather_sdk.py
+++ test_openweather_sdk.py
@@ -1,5 +1,9 @@
 import pytest
 import os
+import requests_mock
+from openweather import OpenWeather
+
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,5 +14,39 @@ def api_key():
     return os.getenv("API_KEY")
 
 
+@pytest.fixture
+def openweather(api_key):
+    return OpenWeather(api_key)
+
+
+@pytest.fixture
+def resposta_api_geolocalizacao():
+    """Sample JSON response that mimics the OpenWeatherMap Geocoding API response for testing."""
+    return [
+        {
+            "name": "Salvador",
+            "lat": -12.9822499,
+            "lon": -38.4812772,
+            "country": "BR",
+            "state": "Bahia",
+        }
+    ]
+
+
 def test_api_key(api_key):
     assert api_key is not None
+
+
+def test_endpoint_geolocalizacao_preenchido(openweather):
+    assert openweather.ENDPOINT_GEOLOCALIZACAO is not None
+    assert len(openweather.ENDPOINT_GEOLOCALIZACAO) > 0
+
+
+def test_obter_latitude_longitude(openweather, resposta_api_geolocalizacao):
+    with requests_mock.Mocker() as mocker:
+        cidade = "Salvador"
+        geocode_url = openweather.construir_url_api_geolocalizacao(cidade)
+        mocker.get(geocode_url, json=resposta_api_geolocalizacao)
+        lat, lon = openweather.obter_latitude_longitude(cidade)
+        assert lat == -12.9822499
+        assert lon == -38.4812772
