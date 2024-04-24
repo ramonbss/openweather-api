1ef92811ae4ef312a8368c77f347c46de39e279c
Ramon Brito <ramon.mecatec@gmail.com>
2024-04-24 15:09:45 -0300

    Testar leitura da chave da API

--- test_openweather_sdk.py
+++ test_openweather_sdk.py
@@ -3,3 +3,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+@pytest.fixture
+def api_key():
+    return os.getenv("API_KEY")
+
+
+def test_api_key(api_key):
+    assert api_key is not None
